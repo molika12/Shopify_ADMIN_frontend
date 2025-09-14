49e791268be57085395f06c6e07df88b516749b6
Y.MOLIKA <163737690+molika12@users.noreply.github.com>
2025-09-14 22:50:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'; // ✅ add Navigate
+import { Routes, Route, Navigate } from 'react-router-dom'; 
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import OrdersPage from "./pages/OrdersPage";
@@ -9,7 +9,7 @@ import SignupPage from './pages/SignupPage';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect / to /login */}
+      <Route path="/" element={<Navigate to="/login" />} /> 
       <Route path="/login" element={<LoginPage />} />
        <Route path="/signup" element={<SignupPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
@@ -17,9 +17,9 @@ export default function App() {
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/settings" element={<SettingsPage />} />
-      {/* Redirect root to login */}
+   
         <Route path="/" element={<Navigate to="/login" />} />
-        {/* Optionally, a 404 page */}
+        
         <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
   );
